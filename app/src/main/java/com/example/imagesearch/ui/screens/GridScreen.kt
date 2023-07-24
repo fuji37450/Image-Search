69f603f051c66f01a9d160c62c658225ae7526dc
fuji37450 <40747030S@ntnu.edu.tw>
2023-07-24 17:52:09 +0800

    split search ui and result ui

---
+++ app/src/main/java/com/example/imagesearch/ui/screens/GridScreen.kt
@@ -0,0 +1,125 @@
+package com.example.imagesearch.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.imagesearch.R
+import com.example.imagesearch.model.Photo
+import com.example.imagesearch.ui.theme.ImageSearchTheme
+
+
+@Composable
+fun GridScreen(
+    photoUiState: PhotoUiState, retryAction: () -> Unit, modifier: Modifier = Modifier
+) {
+    when (photoUiState) {
+        is PhotoUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+        is PhotoUiState.Success -> GridResult(
+            photoUiState.photos, modifier = modifier.fillMaxWidth()
+        )
+        is PhotoUiState.Error -> ErrorScreen(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun GridResult(photos: List<Photo>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(4.dp)
+    ) {
+        items(items = photos, key = { photo -> photo.id }) { photo ->
+            PhotoCard(
+                photo = photo,
+                modifier = modifier
+                    .padding(4.dp)
+                    .fillMaxWidth()
+                    .aspectRatio(1.5f)
+            )
+        }
+    }
+}
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = ""
+    )
+}
+
+/**
+ * The home screen displaying error message with re-attempt button.
+ */
+@Composable
+fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = "失敗", modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text("重試")
+        }
+    }
+}
+
+@Composable
+fun PhotoCard(photo: Photo, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column{
+            AsyncImage(
+                model = ImageRequest.Builder(context = LocalContext.current).data(photo.imgSrc)
+                    .crossfade(true).build(),
+                error = painterResource(R.drawable.ic_broken_image),
+                placeholder = painterResource(R.drawable.loading_img),
+                contentDescription = photo.id,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CardPreview() {
+    ImageSearchTheme {
+        Column(Modifier.padding(vertical = 16.dp)) {
+            val mockData = List(10) { Photo("$it", "") }
+            GridResult(photos = mockData)
+        }
+    }
+}
